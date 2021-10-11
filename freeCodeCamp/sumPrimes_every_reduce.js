bed12f2c37f7be6f3e055f613aff9ed1ad99e474
Astalsi401 <misting401@gmail.com>
2021-10-11 23:15:09 +0800

    update y520

--- freeCodeCamp/sumPrimes_every_reduce.js
+++ freeCodeCamp/sumPrimes_every_reduce.js
@@ -1,7 +1,7 @@
 /*求質數和 */
 function sumPrimes(num) {
     let ans = [];
-    for (let j = 2; j < num; j++) {
+    for (let j = 2; j <= num; j++) {
         if (ans.every(a => j % a !== 0)) ans.push(j)
     }
     return ans.reduce((sum, b) => sum + b)
